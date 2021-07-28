8fddf6576d28b2cd9c19bf0d617db5bf8f920a73
AbdumurodovaZulfizar <abdumurodovazulfizar77@gamil,com>
2021-07-28 11:49:49 +0500

    Correct linter errors

--- script.js
+++ script.js
@@ -1,37 +1,35 @@
-const header = document.querySelector("header");
-const title = document.getElementById("logo");
-const button = document.querySelector(".menu-bars");
+const header = document.querySelector('header');
+const title = document.getElementById('logo');
+const button = document.querySelector('.menu-bars');
 
 // const main = document.querySelector("main");
 
-const navbar = document.querySelector("nav");
-const ul = document.querySelector("ul");
-const button2 = document.createElement("button");
-button2.innerHTML = "<i class='fas fa-times fa-2x'></i>"
+const navbar = document.querySelector('nav');
+const button2 = document.createElement('button');
+button2.innerHTML = `<i class='fas fa-times fa-2x'></i>`;
 
-const link = document.querySelector(".nav-link");
-const link2 = document.querySelector(".link2");
-const link3 = document.querySelector(".link3");
-
-button.addEventListener("click", showNav)
+const link = document.querySelector('.nav-link');
+const link2 = document.querySelector('.link2');
+const link3 = document.querySelector('.link3');
 
 function showNav() {
-        title.remove();
-        button.remove();
-        navbar.classList.remove("none");
-        header.appendChild(navbar);
-        header.appendChild(button2);
+  title.remove();
+  button.remove();
+  navbar.classList.remove('none');
+  header.appendChild(navbar);
+  header.appendChild(button2);
 }
 
-button2.addEventListener("click", showMenu);
-link.addEventListener("click", showMenu);
-link2.addEventListener("click", showMenu);
-link3.addEventListener("click", showMenu);
 
 function showMenu() {
   button2.remove();
-  navbar.classList.add("none");
+  navbar.classList.add('none');
   header.appendChild(title);
   header.append(button);
 }
 
+button.addEventListener('click', showNav)
+button2.addEventListener('click', showMenu);
+link.addEventListener('click', showMenu);
+link2.addEventListener('click', showMenu);
+link3.addEventListener('click', showMenu);
