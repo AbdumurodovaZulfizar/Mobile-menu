282d6205d7d65e7fe5ce93d1e92168583fb652f7
AbdumurodovaZulfizar <abdumurodovazulfizar77@gamil,com>
2021-07-28 21:35:24 +0500

    Change variable name

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ const button = document.querySelector('.menu-bars');
 // const main = document.querySelector("main");
 
 const navbar = document.querySelector('nav');
-const button2 = document.createElement('button');
-button2.innerHTML = '<i class="fas fa-times fa-2x"></i>';
+const cross = document.createElement('button');
+cross.innerHTML = '<i class="fas fa-times fa-2x"></i>';
 
 const link = document.querySelector('.nav-link');
 const link2 = document.querySelector('.link2');
@@ -17,18 +17,18 @@ const showNav = () => {
   button.remove();
   navbar.classList.remove('none');
   header.appendChild(navbar);
-  header.appendChild(button2);
+  header.appendChild(cross);
 };
 
 const showMenu = () => {
-  button2.remove();
+  cross.remove();
   navbar.classList.add('none');
   header.appendChild(title);
   header.append(button);
 }
 
 button.addEventListener('click', showNav);
-button2.addEventListener('click', showMenu);
+cross.addEventListener('click', showMenu);
 link.addEventListener('click', showMenu);
 link2.addEventListener('click', showMenu);
 link3.addEventListener('click', showMenu);
